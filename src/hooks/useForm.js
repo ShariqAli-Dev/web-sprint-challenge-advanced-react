6321912067f8b032ed12a17ce8f167d9aef77613
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-08-15 22:56:47 -0400

    Removed clearForm() as it caused bugs

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,15 +2,11 @@ import { useState } from "react";
 
 // write your custom hook here to control your checkout form
 export default function useForm(initialValue) {
-  const [values, setValues] = useState("formValues", initialValue);
+  const [values, setValues] = useState(initialValue);
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const clearForm = (e) => {
-    setValues(initialValue);
-  };
-
-  return [values, handleChanges, clearForm];
+  return [values, handleChanges];
 }
